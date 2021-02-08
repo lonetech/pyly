07ed5917cedafcaa9f1e4f8febdc31991ffbc0d9
Yann Vernier <yann@vernier.se>
2021-02-08 16:49:08 +0100

    Corrected reading of data over IP.

--- comm_ip.py
+++ comm_ip.py
@@ -3,24 +3,38 @@
 #qpy:console
 
 import socket, struct
+from collections import namedtuple
 import lytro
 
 magic = 0xfaaa55af
+Response = namedtuple('Response', ['magic', 'size', 'seq', 'command'])
 
 class IpTarget(lytro.Target):
     def __init__(self, address):
         self.s = socket.create_connection(address)
         self.s.settimeout(5)
     def read(self, command, size):
-        prefix=struct.pack('<3I', magic, size, 1)
+        prefix=struct.pack(b'<3I', magic, size, 1)
         self.s.send(prefix+command)
-        response=self.s.recv(3*4+len(command)+size)
+        response_s=self.s.recv(3*4+len(command))
+        response=Response(*struct.unpack(b'<3IB', response_s[:13]))
         # Check that the response does match
-        assert response[:3*4+1]==struct.pack('<3Ic', magic, size, 2, command[0])
-        return response[3*4+len(command):]
+        #print(f"Response: {response!r} {response_s!r}")
+        assert response.magic == magic
+        assert response.command == command[0]
+        payload=self.s.recv(response.size)
+        while len(payload)<response.size:
+            payload+=self.s.recv(response.size-len(payload))
+        return payload
     def write(self, command, data):
-        prefix=struct.pack('<3I', magic, size, 0)
+        prefix=struct.pack(b'<3I', magic, len(data), 0)
         self.s.send(prefix+command+data)
+        response=self.s.recv(3*4+len(command))
+        # Check that the response does match
+        #print(f"Response: {response!r}")
+        assert response[:3*4+1]==struct.pack(b'<3IB', magic, len(data), 2, command[0])
+        return response[3*4+len(command):]
 
 def probe():
+    # TODO: Check if it's actually there?
     return [("10.100.1.1", 5678)]
